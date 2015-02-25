d572b311c70d6b6ad6f17629a172fdd5edd5091c
Vatalii Muravitskiy <vmyrik@gmail.com>
2015-02-25 13:36:22 +0200

    languages: add pl, version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='ptime',
-    version='0.1.4',
+    version='0.1.5',
     url='http://github.com/Ibadinov/ptime',
     license='MIT',
     author='Marat Ibadinov',
